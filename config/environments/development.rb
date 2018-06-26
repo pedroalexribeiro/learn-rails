49fa83d15fd677cd77abb8036bba65b1abbe64d5
pedroalexribeiro <pedro.alex.ribeiro@hotmail.com>
2018-06-26 01:30:03 +0100

    dynamic home page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,6 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
   config.action_mailer.smtp_setting = {
     address: "smtp.sendgrid.net",
     port: 587,
@@ -49,8 +48,9 @@ Rails.application.configure do
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application,secrets.email_provider_password
+    password: Rails.application.secrets.email_provider_password
   }
+
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000'}
   config.action_mailer.delivery_method = :smtp
